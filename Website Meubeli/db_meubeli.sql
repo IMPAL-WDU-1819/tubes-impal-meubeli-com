64acbee318308702717cba10f9e239974595b0a1
JaviarFasyah <fasyahj@yahoo.com>
2018-11-18 13:18:50 +0700

    Update db_meubeli.sql

--- Website Meubeli/db_meubeli.sql
+++ Website Meubeli/db_meubeli.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2018 at 04:00 AM
+-- Generation Time: Nov 18, 2018 at 07:17 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -41,7 +41,95 @@ CREATE TABLE `akun` (
 --
 
 INSERT INTO `akun` (`username`, `password`, `nama`, `email`, `hak`) VALUES
-('admin', 'admin', 'ADMIN', 'admin@meubeli.com', 'ADMIN');
+('admin', 'admin', 'ADMIN', 'admin@meubeli.com', 'ADMIN'),
+('crypt', 'cordeon', 'Ja-VR', 'fasyahj@yahoo.com', 'BASIC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cicilan`
+--
+
+CREATE TABLE `cicilan` (
+  `id_transaksi` char(12) NOT NULL,
+  `cicilan` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gambar`
+--
+
+CREATE TABLE `gambar` (
+  `id_meubel` char(12) NOT NULL,
+  `gambar` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gambar`
+--
+
+INSERT INTO `gambar` (`id_meubel`, `gambar`) VALUES
+('m000000001', 'kursi-goyang1.jpg'),
+('m000000001', 'kursi-goyang2.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meubel`
+--
+
+CREATE TABLE `meubel` (
+  `id_meubel` char(10) NOT NULL,
+  `nama_meubel` varchar(50) NOT NULL,
+  `harga_meubel` int(8) NOT NULL,
+  `kategori_meubel` varchar(25) NOT NULL,
+  `jenis_meubel` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `meubel`
+--
+
+INSERT INTO `meubel` (`id_meubel`, `nama_meubel`, `harga_meubel`, `kategori_meubel`, `jenis_meubel`) VALUES
+('m000000001', 'kursi goyang', 500000, 'kursi', 'kursi santai');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sparepart`
+--
+
+CREATE TABLE `sparepart` (
+  `id_sparepart` char(12) NOT NULL,
+  `nama_sparepart` varchar(50) NOT NULL,
+  `harga_sparepart` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` char(12) NOT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `jml_cicilan` int(2) NOT NULL,
+  `jenis_pembayaran` varchar(8) NOT NULL,
+  `status_transaksi` varchar(8) NOT NULL,
+  `id_user` char(10) NOT NULL,
+  `id_meubel` char(10) NOT NULL,
+  `id_sparepart` char(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `tgl_transaksi`, `jml_cicilan`, `jenis_pembayaran`, `status_transaksi`, `id_user`, `id_meubel`, `id_sparepart`) VALUES
+('t00000000001', '2018-11-17', 0, 'cash', 'lunas', 'crypt', 'm000000001', '-');
 
 --
 -- Indexes for dumped tables
@@ -52,6 +140,24 @@ INSERT INTO `akun` (`username`, `password`, `nama`, `email`, `hak`) VALUES
 --
 ALTER TABLE `akun`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `meubel`
+--
+ALTER TABLE `meubel`
+  ADD PRIMARY KEY (`id_meubel`);
+
+--
+-- Indexes for table `sparepart`
+--
+ALTER TABLE `sparepart`
+  ADD PRIMARY KEY (`id_sparepart`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
